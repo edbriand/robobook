5bc61eae33c19a3fcf3bd2ebdf3f714bc9b8d670
Eddy Briand <333ebr@gmail.com>
2022-02-21 21:30:53 +0100

    Updates

---
+++ precache-manifest.66519cd8d9761a541394c34a11669c4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc39f8f578fda62a314607aab6f7fc7",
+    "url": "/robobook/index.html"
+  },
+  {
+    "revision": "9fc785b2fcfbabbe9b62",
+    "url": "/robobook/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "a64a419da37feb33eab0",
+    "url": "/robobook/static/css/main.60c0e73a.chunk.css"
+  },
+  {
+    "revision": "9fc785b2fcfbabbe9b62",
+    "url": "/robobook/static/js/2.b95c5045.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/robobook/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "a64a419da37feb33eab0",
+    "url": "/robobook/static/js/main.88c3d3d0.chunk.js"
+  },
+  {
+    "revision": "ba677a4d35f5d8a4a64b",
+    "url": "/robobook/static/js/runtime~main.b1374191.js"
+  },
+  {
+    "revision": "f073a2b36f5a2ebc64f8b73f539a8639",
+    "url": "/robobook/static/media/Spin Cycle.f073a2b3.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39bc50c2949d344f1280d2802ce8615d.js"
+  "/robobook/precache-manifest.66519cd8d9761a541394c34a11669c4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robobook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
