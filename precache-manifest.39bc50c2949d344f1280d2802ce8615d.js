184f39a3d692c20830c40ee07bec23697c562347
Eddy Briand <333ebr@gmail.com>
2022-02-21 21:28:37 +0100

    Updates

---
+++ precache-manifest.39bc50c2949d344f1280d2802ce8615d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb740633b5e4882f80e46f5aff967d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc785b2fcfbabbe9b62",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "3ce5a2cc5a9af999c3cf",
+    "url": "/static/css/main.302e1eb0.chunk.css"
+  },
+  {
+    "revision": "9fc785b2fcfbabbe9b62",
+    "url": "/static/js/2.b95c5045.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "3ce5a2cc5a9af999c3cf",
+    "url": "/static/js/main.88c3d3d0.chunk.js"
+  },
+  {
+    "revision": "85b68fe97a559349cb7a",
+    "url": "/static/js/runtime~main.0c677e9e.js"
+  },
+  {
+    "revision": "f073a2b36f5a2ebc64f8b73f539a8639",
+    "url": "/static/media/Spin Cycle.f073a2b3.ttf"
+  }
+]);
