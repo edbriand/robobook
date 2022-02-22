e87d1fec076eb91340676b6a1acc5f05aa52dae1
Eddy Briand <333ebr@gmail.com>
2022-02-22 16:30:21 +0100

    replaced random users by REST API fetch, styling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,69 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 import './App.css';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
-}
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then( users =>
+                {
+                    /// Load and instantiate Chance
+                    var chance = require('chance').Chance();
+
+                    for (let i = 0; i < users.length; i++) { 
+                        users[i]["username"] = chance.twitter().slice(1);
+                        users[i]["hash"] = chance.hash({length: 10});
+                        users[i]["color"] = `rgb(${randomColorChan(90,230)},
+                            ${randomColorChan(90,128)},
+                            ${randomColorChan(220,255)})`;
+                    }
+                    
+                    this.setState({robots: users});
+                    console.log(users);
+                }
+            );  
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
 
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className="app">
-                <h1>Robobook</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return (
+                <div className="loading">
+                    <h2>Loading...</h2>
+                </div>
+            )
+        }
+        else
+        {
+            return (
+                <div className="app">
+                    <h1>Robobook</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
+}
+
+function randomColorChan(min,max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export default App;
